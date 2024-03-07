51768e7f253161b16e4c88b420f98ab55e5a8fc8
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-03-06 23:26:25 -0700

    Se crea el caché manager para guardar el caché

---
+++ game-of-thrones/src/main/java/com/vass/got/CacheConfiguration.java
@@ -0,0 +1,18 @@
+package com.vass.got;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfiguration {
+
+	@Bean
+	public CacheManager getManager() {
+		return new ConcurrentMapCacheManager("translations");
+	}
+
+}
