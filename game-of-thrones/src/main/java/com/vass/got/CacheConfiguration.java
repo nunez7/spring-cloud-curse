0d1e6454ef83bdf09a34c04c0b7b50a35ad9ecbc
Felix Nunez <felix.nunez@vass.com.mx>
2024-03-09 11:38:54 -0700

    Se agrega Redison Client a proyecto

--- game-of-thrones/src/main/java/com/vass/got/CacheConfiguration.java
+++ game-of-thrones/src/main/java/com/vass/got/CacheConfiguration.java
@@ -1,18 +1,34 @@
 package com.vass.got;
 
+import java.util.Map;
+import java.util.HashMap;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.cache.CacheConfig;
+import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @EnableCaching
 @Configuration
 public class CacheConfiguration {
+	
+	@Bean(destroyMethod = "shutdown")
+	public RedissonClient redisson() {
+		Config config = new Config();
+		config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+		return Redisson.create(config);
+	}
 
 	@Bean
-	public CacheManager getManager() {
-		return new ConcurrentMapCacheManager("translations");
+	public CacheManager getManager(RedissonClient redissonClient) {
+		Map<String, CacheConfig> config = new HashMap<>();		
+		config.put("translations", new CacheConfig());
+		return new RedissonSpringCacheManager(redissonClient);
 	}
 
 }
