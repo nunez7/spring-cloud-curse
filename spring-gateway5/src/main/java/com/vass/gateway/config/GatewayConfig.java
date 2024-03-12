d6e0e88e809085cb1c0b77af073fb741dfb21cc0
Felix Nunez <felix.nunez@vass.com.mx>
2024-03-12 14:12:44 -0700

    Integrando el filtro a las rutas del gateway

--- spring-gateway5/src/main/java/com/vass/gateway/config/GatewayConfig.java
+++ spring-gateway5/src/main/java/com/vass/gateway/config/GatewayConfig.java
@@ -1,5 +1,6 @@
 package com.vass.gateway.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,9 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GatewayConfig {
+	
+	@Autowired
+	private AuthFilter authFilter;
 
 	@Bean
 	@Profile("localhostRouter-noEureka")
@@ -31,16 +35,23 @@ public class GatewayConfig {
 	public RouteLocator configLocalEurekaCircuitBraker(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(r -> r.path("/api/v1/dragonball/*")
-						.filters( f -> 
+						.filters( f -> {
 							f.circuitBreaker(
 									c -> c.setName("failoverCB")
 									.setFallbackUri("forward:/api/v1/db-failover/characters")
-									.setRouteId("dbFailover")))
+									.setRouteId("dbFailover"));
+							f.filter(authFilter);
+							return f;
+						})
 						.uri("lb://dragon-ball"))
 				.route(r -> r.path("/api/v1/db-failover/*")
+						.filters(f->f.filter(authFilter))
 						.uri("lb://dragon-ball-failover"))
 				.route(r -> r.path("/api/v1/game-of-thrones/*")
+						.filters(f->f.filter(authFilter))
 						.uri("lb://game-of-thrones"))
+				.route(r->r.path("/auth/**")
+						.uri("lb://nunez-oauth-service"))
 				.build();
 	}
 
