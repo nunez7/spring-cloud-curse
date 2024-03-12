fb817cd9ff2eb331736b7fed09f13c52075be5dd
Felix Nunez <felix.nunez@vass.com.mx>
2024-03-12 13:48:33 -0700

    Creacion de DTO de login y webclient

---
+++ spring-gateway5/src/main/java/com/vass/gateway/config/WebClientConfig.java
@@ -0,0 +1,15 @@
+package com.vass.gateway.config;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+	
+	@LoadBalanced
+	@Bean
+	public WebClient.Builder webclient(){
+		return WebClient.builder();
+	}
+}
